1e0ce4bd12c2a94d37c95534ab570a694a2a49cd
maringan <kamil.kucharski88+1@gmail.com>
2014-02-10 23:31:19 +0100

    add more validation

--- spec/app/models/user_spec.rb
+++ spec/app/models/user_spec.rb
@@ -4,4 +4,5 @@ describe User do
   it { should validate_presence_of :email  }
   it { should validate_presence_of :iden   }
   it { should validate_uniqueness_of :iden }
+  it { should validate_uniqueness_of :email }
 end
