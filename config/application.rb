2e8d528a4cc25adc539903f7806a953c34a8ebd7
maringan <kamil.kucharski88+1@gmail.com>
2014-02-10 07:47:52 +0100

    and more

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ require_relative 'environment'
 
 Bundler.require :default, ENV['RACK_ENV']
 
-Dir[File.expand_path('../../app/api/*/*.rb', __FILE__),
+Dir[File.expand_path('../../app/api/*/base.rb', __FILE__),
+    File.expand_path('../../app/api/*/v*.rb', __FILE__),
     File.expand_path('../../app/model/*.rb', __FILE__)].each do |f|
   require f
 end
