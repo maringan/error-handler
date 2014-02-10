e4f6b695d7595f937ef6096c4b407846798a4262
maringan <kamil.kucharski88+1@gmail.com>
2014-02-10 07:37:37 +0100

    heroku fix problem

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 require 'yaml'
 require 'active_record'
+require 'erb'
 
 env = (ENV['RACK_ENV'] || :development)
 
-db_config = YAML.load(File.read('config/database.yml'))[env]
+db_config = YAML.load(ERB.new(File.read('config/database.yml')).result)[env]
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.establish_connection(db_config)
